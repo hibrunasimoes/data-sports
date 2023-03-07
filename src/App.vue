747c84646ad089e70ff3d384b629ba761cd1c36c
bruna simoes moita <bruna.simoesmoita@gmail.com>
2023-03-07 18:46:51 -0300

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBarComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBarComponent  from './components/NavBarComponent';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    NavBarComponent
   }
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 </style>
+
---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,67 @@
+<template>
+   <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand" href="#">DataEL<span> {</span>AS<span>} </span></a>
+      <img src = "" alt = "">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Servicos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link me-5" href="#">Esportes</a>
+          </li>
+        </ul>
+        <a href="#" class ="btn btn-purple ">
+          Teste gratuitamente</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBarComponent'
+}
+</script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans&display=swap');
+
+.navbar .btn-outline-secondary {
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  padding: 10px 16px;
+}
+.nav-link {
+  margin-left: 41px;
+  font-size: 20px;
+}
+.navbar{
+  padding: 32px 0;
+}
+.nav-link {
+  margin-left: 41px;
+  font-size: 16px;
+}
+.nav-link .nav-link.active {
+  font-weight: bold;
+}
+.btn-purple {
+  color: #fff;
+  background-color: #6f42c1;
+  border-color: #61129F;
+}
+span{
+  display: inline-block;
+  text-decoration:none;
+  color: inherit;
+  color: #61129F;
+}
+</style>
