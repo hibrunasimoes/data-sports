7b17e9d007620c8bfa5324d9d514bdcc7b06c10b
bruna simoes moita <bruna.simoesmoita@gmail.com>
2023-03-14 10:27:11 -0300

    refactoring table and add json server

--- src/pages/EmployeesPage.vue
+++ src/pages/EmployeesPage.vue
@@ -11,104 +11,36 @@
             <th>Title</th>
             <th>Status</th>
             <th>Position</th>
-            <th>Actions</th>
           </tr>
         </thead>
-        <tbody>
+         <tbody v-for="employe in employess" :key="employe.name">
           <tr>
             <td>
               <div class="d-flex align-items-center">
-                <img
-                     src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                <img 
                      alt=""
                      style="width: 45px; height: 45px"
                      class="rounded-circle"
-                     />
+                     :src="employe.adressImage"
+                     /> 
                 <div class="ms-3">
-                  <p class="fw-bold mb-1">Bruna Simoes</p>
-                  <p class="text-muted mb-0">bruna.simoesdoe@gmail.com</p>
+                  <p class="fw-bold mb-1">{{employe.name}}</p>
+                  <p class="text-muted mb-0">{{employe.email}}</p>
                 </div>
               </div>
             </td>
-            <td>
-              <p class="fw-bold mb-1">Software engineer</p>
-              <p class="text-muted mb-0">IT department</p>
+              <td>
+              <p class="fw-bold mb-1">{{employe.role}}</p>
+              <p class="text-muted mb-0">{{employe.department}}</p>
             </td>
             <td>
-              <span class="badge bg-success rounded-pill " style="opacity: 0.6">Active</span>
+              <span class="badge bg-success rounded-pill " style="opacity: 0.6">{{employe.status}}</span>
             </td>
-            <td>Senior</td>
-            <td>
-              <button type="button" class="btn btn-link btn-sm btn-rounded">
-                Edit
-              </button>
-            </td>
+            <td>{{employe.position}}</td>
           </tr>
           <tr>
-            <td>
-              <div class="d-flex align-items-center">
-                <img
-                     src="https://mdbootstrap.com/img/new/avatars/6.jpg"
-                     class="rounded-circle"
-                     alt=""
-                     style="width: 45px; height: 45px"
-                     />
-                <div class="ms-3">
-                  <p class="fw-bold mb-1">Alex Ray</p>
-                  <p class="text-muted mb-0">alex.ray@gmail.com</p>
-                </div>
-              </div>
-            </td>
-            <td>
-              <p class="fw-normal mb-1">Consultant</p>
-              <p class="text-muted mb-0">Finance</p>
-            </td>
-            <td>
-              <span class="badge bg-primary rounded-pill " style="opacity: 0.3">Onboarding</span>
-            </td>
-            <td>Junior</td>
-            <td>
-              <button
-                      type="button"
-                      class="btn btn-link btn-rounded btn-sm fw-bold"
-                      data-mdb-ripple-color="dark"
-                      >
-                Edit
-              </button>
-            </td>
           </tr>
           <tr>
-            <td>
-              <div class="d-flex align-items-center">
-                <img
-                     src="https://mdbootstrap.com/img/new/avatars/7.jpg"
-                     class="rounded-circle"
-                     alt=""
-                     style="width: 45px; height: 45px"
-                     />
-                <div class="ms-3">
-                  <p class="fw-bold mb-1">Bruna Simoes</p>
-                  <p class="text-muted mb-0">bruna.simoes@gmail.com</p>
-                </div>
-              </div>
-            </td>
-            <td>
-              <p class="fw-normal mb-1">Tech Leader</p>
-              <p class="text-muted mb-0">TI</p>
-            </td>
-            <td>
-              <span class="badge bg-warning rounded-pill" style="opacity: 0.6" >Awaiting</span>
-            </td>
-            <td>Senior</td>
-            <td>
-              <button
-                      type="button"
-                      class="btn btn-link btn-rounded btn-sm fw-bold"
-                      data-mdb-ripple-color="dark"
-                      >
-                Edit
-              </button>
-            </td>
           </tr>
         </tbody>
       </table>
@@ -118,6 +50,20 @@
 
 <script>
 export default {
-  name: "EmployeesPage"
+  name: "EmployeesPage",
+    data () {
+    return {
+      employess:[],
+    }
+  },
+  async mounted() {
+  try {
+    const response = await fetch('http://localhost:3000/employess');
+    const data = await response.json();
+    this.employess = data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 }
 </script>
